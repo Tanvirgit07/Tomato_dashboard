2557c83a7ec2dd0aecece3d2db5b43d344ca7276
Tanvirgit07 <ahmmedtanvir9090@gmail.com>
2025-08-26 13:39:30 +0600

    new

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+// next.config.mjs
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ["res.cloudinary.com", "images.unsplash.com"],
+  },
+};
+
+export default nextConfig;
